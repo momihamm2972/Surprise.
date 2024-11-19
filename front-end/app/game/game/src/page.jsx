7472b2c232ed05e6af2616c3bd47a8fba1f5a5e6
Mohamed Mihammi <momihamm@e2r5p1.1337.ma>
2024-11-19 20:57:02 +0100

    feat : the background is ready to use

--- front-end/app/game/game/src/page.jsx
+++ front-end/app/game/game/src/page.jsx
@@ -6,7 +6,7 @@
 /*   By: momihamm <momihamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 12:41:28 by momihamm          #+#    #+#             */
-/*   Updated: 2024/11/19 20:23:54 by momihamm         ###   ########.fr       */
+/*   Updated: 2024/11/19 20:55:31 by momihamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,44 @@ function MyButton()
 function Back() {
     return (
         // <div style={{ backgroundColor: '#020207', height: '100vh' }}>
-        <div style={{ background: 'linear-gradient(to Left, #020207, #1c3986, #020207)', height: '100vh' }}>
-            {/* Your content goes here */}
-        </div>
+        <div style={{ 
+            background: `radial-gradient(circle at bottom, #020207 5%, transparent 80%), linear-gradient(to left, #020207, #1c3986, #020207)`,
+            height: '100vh' 
+        }}>
+            </div>
     );
 }
 
+// function Back() {
+//     return (
+//         <div style={{
+//             background: `
+//                 radial-gradient(circle at top, black 50%, transparent 80%),
+//                 linear-gradient(to Left, black 30%, transparent 31%, transparent 80%, black 81%)`,
+//             height: '100vh'
+//         }}>
+//             {/* Your content goes here */}
+//         </div>
+//     );
+// }
+
+
+
+
+// function Back() {
+//     return (
+//         <div style={{ 
+//             background: 'conic-gradient(black 0deg 90deg, white 90deg 270deg, black 270deg 360deg)',
+//             height: '100vh',
+//             width: '100%',
+//             display: 'flex',
+//             justifyContent: 'center',
+//             alignItems: 'center'
+//         }}>
+//             {/* Your content goes here */}
+//         </div>
+//     );
+// }
 
 function App()
 {
@@ -57,3 +89,7 @@ function App()
 }
 
 export default App;
+
+
+
+// }
